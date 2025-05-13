92619037fc70a9fea957eb90b72ebbf74875665d
mizoham <elhimhamza7@gmail.com>
2025-05-13 18:26:19 +0100

    enable cors

---
+++ src/main/java/com/geology_platform/geology/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.geology_platform.geology.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author ELHIM Hamza
+ **/
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000","http://localhost:5173","http://localhost:5174","http://localhost:5175")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .exposedHeaders("Authorization", "Content-Type")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
